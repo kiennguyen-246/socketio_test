b7037f8113ef23bc310eb33c570522bc40221df0
Kien Nguyen <85871564+kiennguyen2464@users.noreply.github.com>
2023-06-26 17:27:48 +0700

    .

---
+++ main.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+
+const app = express();
+const server = http.createServer(app)
+const io = new Server(server)
+
+let usernameList = [];
+
+app.get('/', (req, res) => {
+  res.sendFile('C:/Users/a/Documents/MEGA/MEGAsync/code/using_files/socketio_test/login.html');
+});
+
+app.get('/chat', (req, res) => {
+  res.sendFile('C:/Users/a/Documents/MEGA/MEGAsync/code/using_files/socketio_test/chat.html');
+});
+
+io.on('connection', (socket) => {
+  console.log(`${username} connected`);
+  io.emit('system message', `${username} connected.`)
+  socket.on('disconnect', () => {
+    console.log(`${username} disconnected`);
+    io.emit('system message', `${username} disconnected.`)
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', `${username}: ${msg}`);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+io.on('connection', (socket) => {
+  socket.on('logged in', (__username) => {
+    username = __username;
+  }) 
+})
